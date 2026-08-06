18d5df30a2376ffe6bfdd5d25a119a3907539daa
leaf0001 <2605@iona.vic.edu.au>
2026-08-06 12:38:19 +1000

    Create Student.js

---
+++ Code/backend/src/models/Student.js
@@ -0,0 +1,10 @@
+export default class Student {
+ constructor(id, name, email, nfcTagId, currentLocation, currentClass) {
+ this.id = id;
+ this.name = name;
+ this.email = email;
+ this.nfcTagId = nfcTagId;
+ this.currentLocation = currentLocation;
+ this.currentClass = currentClass;
+ }
+}

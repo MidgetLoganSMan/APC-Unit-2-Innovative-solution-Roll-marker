6272c9598080e46e2de3f9ed4c9a62f5da12d02c
leaf0001 <2605@iona.vic.edu.au>
2026-08-10 13:51:23 +1000

    Create class.js

---
+++ Code/backend/src/models/Class.js
@@ -0,0 +1,9 @@
+export default class Class {
+ constructor(id, className, teacherId, room, period) {
+    this.id = id;
+    this.className = className;
+    this.teacherId = teacherId;
+    this.room = room;
+    this.period = period;
+ }
+}

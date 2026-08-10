c8c52b10520c73038b1250be82ebb061b0751490
leaf0001 <2605@iona.vic.edu.au>
2026-08-10 13:53:13 +1000

    Create timetable.js

---
+++ Code/backend/src/models/timetable.js
@@ -0,0 +1,8 @@
+export default class Timetable {
+ constructor(id, studentId, classId, period) {
+ this.id = id;
+ this.studentId = studentId;
+ this.classId = classId;
+ this.period = period;
+ }
+}
